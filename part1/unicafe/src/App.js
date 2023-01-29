39727922d00fbcabc4240140181cbad8206e4a54
Antonio Lopez <antoniolopez7217@gmail.com>
2023-01-29 15:18:19 +0100

    unicafe 1.11 finished

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,31 +4,41 @@ const Header = ({text}) => <h1>{text}</h1>
 
 const Button = (props) => <button onClick={props.handleClick}>{props.feedback}</button>
 
-const Statistics = ({feedback, counter}) => {
-  if (counter[3] === 0) {return ("No feedback given")}
-  return (
-    <div>
-      <p>{feedback[0]} {counter[0]}</p>
-      <p>{feedback[1]} {counter[1]}</p>
-      <p>{feedback[2]} {counter[2]}</p>
-      <p>{feedback[3]} {counter[3]}</p>
-      <p>{feedback[4]} {counter[4]}</p>
-      <p>{feedback[5]} {counter[5]}</p>
+const Statistics = (props) => {
+  const good = props.feedback[0]
+  const neutral = props.feedback[1]
+  const bad = props.feedback[2]
+  const all = good + bad + neutral
+  const average = (good - bad)/all
+  const positive = (good / (bad + neutral + good))*100 + " %"
 
-    </div>
+  if (all === 0) {return ("No feedback given")}
+  return (
+    <table>
+      <tbody>
+        < StatisticLine text="good" value={good}/>
+        < StatisticLine text="neutral" value={neutral}/>
+        < StatisticLine text="bad" value={bad}/>
+        < StatisticLine text="all" value={all}/>
+        < StatisticLine text="average" value={average}/>
+        < StatisticLine text="positive" value={positive}/>
+      </tbody>
+    </table>
   )
 }
+
+const StatisticLine = (props) => (
+<tr>
+  <td>{props.text}</td>
+  <td>{props.value}</td>
+</tr>
+)
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const all = good + bad + neutral
-  const average = (good - bad)/all
-  const positive = (good / (bad + neutral + good))*100 + " %"
-
   return (
     <div>
       <Header text={"give feedback"}/>
@@ -36,10 +46,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} feedback={"neutral"}/>
       <Button handleClick={() => setBad(bad + 1)} feedback={"bad"}/>
       <Header text={"statistics"}/>
-      <Statistics 
-      feedback={["good","neutral","bad","all","average","positive"]} 
-      counter={[good, neutral, bad,all,average,positive]}
-      />
+      <Statistics feedback={[good,neutral,bad]}/>
     </div>
   )
 }

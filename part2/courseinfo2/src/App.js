bb9598c87c1ae925ac118a4ca1be537de53730df
Antonio Lopez <antoniolopez7217@gmail.com>
2023-02-01 20:14:04 +0100

    exercise 2.3 finished

--- part2/courseinfo2/src/App.js
+++ part2/courseinfo2/src/App.js
@@ -6,14 +6,16 @@ const Course = ({course})=> {
   const Content = ({parts}) => parts.map((part) => <Part key={part.id} part={part}/>)
 
   const Part = ({part}) => <p>{part.name} {part.exercises}</p>
-
-  //const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+  
+  const Total = () => <p ><strong>total of {SumExercises} exercises</strong></p>
+  
+  const SumExercises = course.parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      {/* <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} /> */}
+      <Total />
     </div>
   )
 
@@ -38,6 +40,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }

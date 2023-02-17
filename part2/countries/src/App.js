1e7269446422fac1131f00b1d4ca7d9f77690931
Antonio Lopez <antoniolopez7217@gmail.com>
2023-02-17 21:27:55 +0100

    exercises 2.18-2.20 (countries) pending to be finished

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,27 +2,25 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [value, setValue] = useState('')
+  const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([]) 
 
   useEffect(() => {
       axios
         .get(`https://restcountries.com/v3.1/all`)
         .then(response => {
-          setCountries(response.data.name)
+          setCountries(response.data)
         })
   }, [])
 
-  console.log(countries.map(x => x.common))
-
   const handleChange = (event) => {
-    setValue(event.target.value)
+    setSearch(event.target.value)
   }
-    
+
   return(
     <div>
       <form>
-        find countries: <input value={value} onChange={handleChange}/>
+        find countries: <input value={search} onChange={handleChange}/>
       </form>
           <p>{countries}</p>  
     </div>

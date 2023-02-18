a6adba5b4867bc02b96421bc0d0054b6a9820e38
Antonio Lopez <antoniolopez7217@gmail.com>
2023-02-18 17:59:50 +0100

    exercise 3.3 finished

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -37,9 +37,16 @@ app.get('/api/info', (request, response) => {
 	response.send(infoPeople + date)
 })
 
-// app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find(person => person.id === id)
 
-// })
+	if (person){
+		response.json(person)
+	} else {
+		response.status(404).end()
+	}
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
